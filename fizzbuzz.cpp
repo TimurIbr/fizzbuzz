b3a7aa28cb7f2fe029b5f1babf5fc17eb7acec20
TimurIbr <36293367+TimurIbr@users.noreply.github.com>
2019-05-20 01:57:24 +0300

    output formation changed
    Changes in defenition of numbers. (a1, 2, a3a, 4a4, and so on - there is no numbers now) 
    The code become more open-closed due to addition of output formation function.

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,6 +1,8 @@
 #include <regex>
 #include <iostream>
 #include <string>
+const std:: string multiple_3_word = "FIZZ";
+const std:: string multiple_5_word = "BUZZ";
 int is_multiple_5 (std::string exam_string){
 	if(exam_string.length() == 0){
 		return -1;
@@ -21,7 +23,7 @@ int is_multiple_3 (std::string exam_string){
 	if(exam_string.length() == 0){
 		return -1;
 	}
-  for(i = 0; i < exam_string.length(); ++i){
+  for(int i = 0; i < exam_string.length(); ++i){
     if( !isdigit(exam_string[i]))
       return -1;
   }
@@ -42,24 +44,27 @@ int is_multiple_3 (std::string exam_string){
 	return -1;
 }
 
+void output_forming(std::string exam_str, std::string* out_str){
+    *out_str = "";
+    if(is_multiple_3(exam_str))
+        *out_str += multiple_3_word;
+    if(is_multiple_5(exam_str))
+        *out_str += multiple_5_word;
+	if(out_str->length() == 0)
+        *out_str = exam_str;
+}
+
 int main(int ac, char ** av){
-    const std:: string multiple_3_word = "FIZZ";
-    const std:: string multiple_5_word = "BUZZ";
 	int i;
 	std::string inp_str, out_str, exam_str;
 	getline(std::cin, inp_str, '\n');
 	std::smatch numbers;
-	std::regex reg("([0-9]+)"); 
-	std:: regex_search(inp_str, numbers, reg);
-	while(std:: regex_search(inp_str, numbers, reg)){
-	    out_str = "";
+	std::regex reg_not_numbers("([\\w]*[a-zA-Z]+[0-9]+[\\w]*|[\\w]*[0-9]+[a-zA-Z]+[\\w]*)"); 
+	inp_str = std:: regex_replace(inp_str, reg_not_numbers, "");
+	std::regex reg_numbers("([0-9]+)"); 
+	while(std:: regex_search(inp_str, numbers, reg_numbers)){
 	    exam_str = numbers[1];
-	    if(is_multiple_3(exam_str))
-	        out_str += multiple_3_word;
-	    if(is_multiple_5(exam_str))
-	        out_str += multiple_5_word;
-	    if(out_str.length() == 0)
-	        out_str = exam_str;
+	    output_forming(exam_str, &out_str);
 	    std::cout << out_str << std::endl;
 	    inp_str = numbers.suffix().str();
 	}
