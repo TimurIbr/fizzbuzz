7ba7206cbcd99cc09d42fc851879273eb0cf37dc
TimurIbr <36293367+TimurIbr@users.noreply.github.com>
2019-05-20 02:27:12 +0300

    hadr for fizzbuzz on cpp
    headr file for fizzbuzz on cpp

---
+++ fizzbuzz.h
@@ -0,0 +1,10 @@
+#ifndef _fizzbuzz_
+#define _fizzbuzz_
+const std:: string multiple_3_word = "FIZZ";
+const std:: string multiple_5_word = "BUZZ";
+int is_multiple_5 (std::string exam_string);
+int is_multiple_3 (std::string exam_string);
+void output_forming(std::string exam_str, std::string* out_str);
+void fizzbuzz_ing(std::string inp_str, std::string* gen_out_str);
+
+#endif
