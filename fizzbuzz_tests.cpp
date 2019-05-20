8172dc44337fbdfb41247f032e99a97b8396f74a
TimurIbr <36293367+TimurIbr@users.noreply.github.com>
2019-05-20 12:43:22 +0300

    tests_log added and negative numbrs testing added
    Now tests progress writes into tests_log.txt;
    Now there are some tests for negative numbers;

--- fizzbuzz_tests.cpp
+++ fizzbuzz_tests.cpp
@@ -7,19 +7,19 @@
 int main(int ac, char ** av){
 	int i;
 	const int num_of_cases = 5;
-  std::string test_case[num_of_cases];
-  std::string test_case_out[num_of_cases];
-  std::string test_case_answer[num_of_cases];
-  test_case[0] = "1       90";
-  test_case_answer[0] = "1 #@ ";
-  test_case[1] = "1 2 3 4 5 6 7 8 9 10";
-  test_case_answer[1] = "1 2 # 4 @ # 7 8 # @ ";
-  test_case[2] = "12 25 37 49 56 90";
-  test_case_answer[2] = "# @ 37 49 56 #@ ";
-  test_case[3] = "12345689 123456789 123456890 1234567890 ";
-  test_case_answer[3] = "12345689 # @ #@ ";
-  test_case[4] = "999999999999999999999999999999999999999";
-  test_case_answer[4] = "# ";
+  	std::string test_case[num_of_cases];
+  	std::string test_case_out[num_of_cases];
+  	std::string test_case_answer[num_of_cases];
+ 	 test_case[0] = "-1       90";
+  	test_case_answer[0] = "-1 #@ ";
+  	test_case[1] = "1 -2 -3 4 -5 6 7 -8 9 10";
+  	test_case_answer[1] = "1 -2 # 4 @ # 7 -8 # @ ";
+  	test_case[2] = "12 25 -37 49 56 90";
+  	test_case_answer[2] = "# @ -37 49 56 #@ ";
+  	test_case[3] = "12345689 123456789 123456890 1234567890 ";
+  	test_case_answer[3] = "12345689 # @ #@ ";
+  	test_case[4] = "999999999999999999999999999999999999999";
+  	test_case_answer[4] = "# ";
 	std::regex reg_into_andl_ ("[\\s]");
 	std::regex reg_into_FIZZ ("(#)");
 	std::regex reg_into_BUZZ ("(@)");
@@ -29,9 +29,19 @@ int main(int ac, char ** av){
 	   test_case_answer[i] = std:: regex_replace(test_case_answer[i], reg_into_BUZZ, multiple_5_word);
 	   test_case_answer[i] = std:: regex_replace(test_case_answer[i], reg_into_endl, "\n");
 	}
-	for(i = 0; i < num_of_cases; ++i){
-	   fizzbuzz_ing(test_case[i], test_case_out + i);
-	   assert((test_case_answer[i] == test_case_out[i]) && "FIZZBUZZ fault") ;
+	std::ofstream tests_log;
+    	tests_log.open("tests_log.txt");
+    	if (!tests_log.is_open())
+       	std::cerr << "No logfiles" << std::endl;
+   	for (i = 0; i < num_of_cases; ++i){
+        	tests_log << "\tcase # " << i << std::endl;
+        	fizzbuzz_ing (test_case[i], test_case_out + i);
+        	tests_log << "\tcase input:\n" << test_case[i] << std::endl;
+        	tests_log << "\texpecting answer:\n" << test_case_answer[i] << std::endl;
+        	tests_log << "\tprogram out:\n" << test_case_out[i] << std::endl; 
+        	assert ((test_case_answer[i] == test_case_out[i]) && "FIZZBUZZ fault: see ./tests_log.txt");
+       		tests_log << "\tcase # " << i << " Succed" << std::endl;
+      		//std::cout << test_case_out[i] << "\n";
 	}
 	return 0;
 }
