39b4decac6cdbd3d6c53ba40b3375a2003d9c6b5
TimurIbr <36293367+TimurIbr@users.noreply.github.com>
2019-05-30 01:02:08 +0300

    comments added
    Added some simple dicription of functions and constants.

--- fizzbuzz.hpp
+++ fizzbuzz.hpp
@@ -3,11 +3,19 @@
 #include <regex>
 #include <iostream>
 #include <string>
-const std:: string multiple_3_word = "FIZZ";
-const std:: string multiple_5_word = "BUZZ";
-int is_multiple_5 (std::string exam_string);
-int is_multiple_3 (std::string exam_string);
-void output_forming(std::string exam_str, std::string* out_str);
+const std:: string multiple_3_word = "FIZZ"; 
+/* this word will replace divisible by 3 numbers in output*/
+const std:: string multiple_5_word = "BUZZ"; 
+/* this word will replace divisible by 5 numbers in output*/
+int is_multiple_5 (std::string exam_string); 
+/* this function checks if number (presented as string) is divisible by 5 or not */
+int is_multiple_3 (std::string exam_string); 
+/* this function checks if number (presented as string) is divisible by 3 or not*/
+void output_forming(std::string exam_str, std::string* out_str); 
+/* this function prints in  out_str  word, which should be outpoted in fizzbuzz instead of exam_str 
+(in fact it might be exam_str)*/
 void fizzbuzz_ing(std::string inp_str, std::string* gen_out_str);
+/* this function  prints in  gen_out_str  changed word sequence, 
+which was recived as  inp_str . Each word in the sequence will be changed by  output_forming  function.*/
 
 #endif
